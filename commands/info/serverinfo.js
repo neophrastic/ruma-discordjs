ac71bf2bd1c4b6949d0a097a7201dd6f6bcb6231
BagoesPantera <bagoespantera01@gmail.com>
2023-02-21 13:07:03 +0800

    add new serverinfo command

---
+++ commands/info/serverinfo.js
@@ -0,0 +1,31 @@
+const {EmbedBuilder} = require('discord.js')
+
+module.exports = {
+    name:'serverinfo',
+    description:'get server information',
+
+    //execute
+    callback: async (client, interaction) => {
+        const { guild } = interaction
+        const { createdTimestamp, ownerId, description, name, memberCount } = guild
+        const icon = guild.icon ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png` : 'https://cdn.discordapp.com/attachments/1077097920492482560/1077448510275858462/default.png'
+        const roles = guild.roles.cache.size
+        const emojis = guild.emojis.cache.size
+
+        const embed = new EmbedBuilder()
+            .setColor("Random")
+            .setAuthor({name: guild.name, iconURL:icon})
+            .setThumbnail(icon)
+            .addFields({name: `Name`, value: `${guild.name}`, inline: false})
+            .addFields({name: `Date created`, value: `<t:${parseInt(createdTimestamp / 1000)}:R> (hover for complete date)`, inline: true})
+            .addFields({name: 'Server owner', value: `<@${ownerId}>`, inline: true})
+            .addFields({name: 'Server member', value: `${memberCount}`, inline: true})
+            .addFields({name: 'Role number', value: `${roles}`, inline: true})
+            .addFields({name: 'Emoji number', value: `${emojis}`, inline: true})
+            .addFields({name: 'Server boost', value: `${guild.premiumSubscriptionCount}`, inline: true})
+            .setFooter({text: `Server ID : ${guild.id}`})
+            .setTimestamp()
+
+        await interaction.reply({embeds: [embed]})
+    }
+}
